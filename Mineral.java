bf174a2a159564606a4f618523d5702fc3777281
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-10-24 16:32:14 -0300

    Update Mineral.java

--- Mineral.java
+++ Mineral.java
@@ -2,70 +2,18 @@ package tp;
 
 
 public class Mineral implements Bloque {
+	public enum tipoDeMineral{COBRE,BRONCE,PLATA,ORO,HIERRO,DIAMANTE};
 	//Diamante debería ser inusual :P
 	private tipoDeMineral tipoMineral;
 	private int precio;
 	private Posicion posicion;
 	private char letra;
 	
-	
-	private void setLetra(tipoDeMineral tipoMineral) {
-		if(tipoMineral == tipoDeMineral.COBRE) {
-			this.letra = 'C';
-		}
-		else if(tipoMineral == tipoDeMineral.BRONCE) {
-			this.letra = 'B';
-		}
-		else if(tipoMineral == tipoDeMineral.HIERRO) {
-			this.letra = 'H';
-		}
-		else if(tipoMineral == tipoDeMineral.PLATA) {
-			this.letra = 'P';
-		}
-		else if(tipoMineral == tipoDeMineral.ORO) {
-			this.letra = 'O';
-		}
-		else {
-			//throw exception
-		}
-	}
-	
-	
-//Acá hay muchos if, hay que aplicar algún patrón quizás :P
-	private int setPrecio(tipoDeMineral tipoMineral) {
-		if(tipoMineral == tipoDeMineral.COBRE) {
-			return 50;
-		}
-		else if(tipoMineral == tipoDeMineral.BRONCE) {
-			return 60;
-		}
-		else if(tipoMineral == tipoDeMineral.HIERRO) {
-			return 30;
-		}
-		else if(tipoMineral == tipoDeMineral.PLATA) {
-			return 50;
-		}
-		else if(tipoMineral == tipoDeMineral.ORO) {
-			return 50;
-		}
-		else {
-			return -1;
-		}
-	}
-	
-//ta raro perdón
-	private boolean esTipoValido(tipoDeMineral mineral) {
-		return (mineral == tipoDeMineral.COBRE || mineral == tipoDeMineral.BRONCE || mineral == tipoDeMineral.HIERRO || mineral == tipoDeMineral.PLATA || mineral == tipoDeMineral.ORO);
-	}
-	
-	
-	public Mineral(tipoDeMineral tipoMineral) {
-		if(!esTipoValido(tipoMineral)) {
-			//throw an exception for invalid type
-		}
-		this.precio = setPrecio(tipoMineral);
+	public Mineral(tipoDeMineral tipoMineral,int precio,char letra) {
+		this.precio = precio;
 		this.tipoMineral = tipoMineral;
-		setLetra(tipoMineral);		
+		this.letra = letra;
+		this.posicion = null; //Esto se debería setear después si es que es necesario tener la posición :P
 	}
 	
 	public int getPrecio() {
@@ -80,3 +28,4 @@ public class Mineral implements Bloque {
 		return this.letra;
 	}
 }
+
