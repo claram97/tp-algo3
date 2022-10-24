5cc03d9c8a9b830616ffcfadd31c9cdb58d93e00
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-10-24 14:28:34 -0300

    Delete Bloque.java

--- Bloque.java
+++
@@ -1,8 +0,0 @@
-package tp;
-
-public interface Bloque {
-	public enum tipoDeMineral{COBRE,BRONCE,PLATA,ORO,HIERRO,DIAMANTE};
-	public tipoDeMineral getTipo();
-	public int getPrecio();
-	public char getLetra();
-}
