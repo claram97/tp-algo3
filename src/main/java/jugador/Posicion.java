108993c44b85f359309f30e2c91140f289e275df
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-11-10 12:50:08 -0300

    Update Posicion.java

--- src/main/java/jugador/Posicion.java
+++ src/main/java/jugador/Posicion.java
@@ -12,18 +12,22 @@ public class Posicion {
 		this.posicionY = posicionY;
 	}
 	
+	//Devuelve la componente X de la posicion
 	public int getX() {
 		return posicionX;
 	}
-
+	
+	//Devuelve la componente Y de la posicion
 	public int getY() {
 		return posicionY;
 	}
 
+	//Devuelve la posicion actual.
 	public Posicion get() {
-		return this;
+		return new Posicion(this.getX(),this.getY());
 	}
 	
+	//Setea la componente X si es positiva.
 	public void setX(int posicionX) {
 		if(posicionX < 0) {
 			//throw an exception
@@ -31,6 +35,7 @@ public class Posicion {
 		this.posicionX = posicionX;
 	}
 	
+	//Setea la componente Y si es positiva.
 	public void setY(int posicionY) {
 		if(posicionY < 0) {
 			//throw an exception
@@ -38,7 +43,7 @@ public class Posicion {
 		this.posicionY = posicionY;
 	}
 	
-	
+	//Setea las posiciones X e Y si son positivas.
 	public void set(int posicionX, int posicionY) {
 		if(posicionX < 0 || posicionY < 0) {
 			//throw an exception
@@ -49,6 +54,7 @@ public class Posicion {
 		}
 	}
 	
+	//Devuelve true si la posición recibida por parámetro es igual que la actual.
 	public boolean esPosicionIgual(Posicion pos2) {
 		if(this.posicionX == pos2.posicionX && this.posicionY == pos2.posicionY) {
 			return true;
