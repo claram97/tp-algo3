34f10467caf8e29be9d98d73606a83969f7d468d
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-11-10 12:35:14 -0300

    Update Aire.java

--- src/main/java/terreno/Aire.java
+++ src/main/java/terreno/Aire.java
@@ -6,11 +6,15 @@ import jugador.Posicion;
 public class Aire implements Bloque, Entidad{
 	public static final char LETRA = ' ';
 	
+	//Devuelve la letra que representa a la clase
 	public char getLetra() {
 		return Aire.LETRA;
 	}
 
+	//Esto es cualquier cosa. Estamos haciendo algo mal porque no puede ser que interactuar no haga nada y que la posicion te devuelva null cuando se supone que tenga una posicion
+	
 	@Override
+	//Permite la interacción con el Jugador dado.
 	public void interactuar(Jugador jugador) {
 	}
 
