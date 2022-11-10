a790db54d1f216371043e14c9a3c8fe45ebf1683
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-11-10 12:20:16 -0300

    Update EstacionDeServicio.java

--- src/main/java/tiendas/EstacionDeServicio.java
+++ src/main/java/tiendas/EstacionDeServicio.java
@@ -22,6 +22,8 @@ public class EstacionDeServicio implements EstacionDeMantenimiento, Entidad {
 		this.sc = null;
 	}
 	
+	//Calcula la cantidad de combustible que se cargará efectivamente en base a
+	//la cantidad elegida, la cantidad actual de combustible y el nivel máximo que se puede cargar en el tanque.
 	public double cantidadDeCombustible(double cantidadCombustible, double capacidadTanque, double cantidadActual) {
 		if(!LITROS_DISPONIBLES.contains((int)cantidadCombustible)) {
 			return -1;
@@ -38,10 +40,12 @@ public class EstacionDeServicio implements EstacionDeMantenimiento, Entidad {
 		return cantidadCargar;
 	}
 	
+	//Calcula la cantidad de plata que se gastará en base a la cantidad de combustible que se carga.
 	public double cantidadDeDinero(double cantidadDeCombustible) {
 		return cantidadDeCombustible * EstacionDeServicio.PRECIO_COMBUSTIBLE;
 	}
 	
+	//Imprime por pantalla las opciones de carga.
 	private void prompt_nafta() {
 		System.out.println("---------------------------");
 		System.out.println("Indique cuanto quiere cargar: 5, 10, 25, 50 (Litros)");
@@ -49,6 +53,7 @@ public class EstacionDeServicio implements EstacionDeMantenimiento, Entidad {
 		System.out.println("Cantidad: ");
 	}
 	
+	//Permite vender a un jugador dado una cantidad dada de combustible.
 	public void vender(Jugador jugador, double cantidad) {
 		double cantidadCombustible = cantidadDeCombustible(cantidad, jugador.getNave().getCapacidadTanque(), jugador.getNave().getNivelDeCombustible());
 		if(cantidadCombustible == -1) {
@@ -61,6 +66,7 @@ public class EstacionDeServicio implements EstacionDeMantenimiento, Entidad {
 	}
 
 	@Override
+	//Realiza la interacción del Jugador dado con la Tienda actual.
 	public void interactuar(Jugador jugador) {
 		prompt_nafta();
 		this.sc = new Scanner(System.in);
@@ -71,16 +77,19 @@ public class EstacionDeServicio implements EstacionDeMantenimiento, Entidad {
 	}
 
 	@Override
+	//Devuelve la posición de la Tienda actual.
 	public Posicion getPosicion() {
 		return this.posicion;
 	}
 
 	@Override
+	//Devuelve la letra correspondiente a la Tienda actual.
 	public char getLetra() {
 		return '#';
 	}
 
 	@Override
+	//Devuelve el tipo de entidad.
 	public TipoEntidad getTipoEntidad() {
 		return TipoEntidad.TIENDA;
 	}
