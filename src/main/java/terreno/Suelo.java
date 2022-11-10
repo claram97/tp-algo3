07bbdc07ee0d7f353854309a14de417fdbcc3abd
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-11-10 12:54:39 -0300

    Update Suelo.java

--- src/main/java/terreno/Suelo.java
+++ src/main/java/terreno/Suelo.java
@@ -14,18 +14,22 @@ public class Suelo {
 		
 	}
 	
+	//Devuelve el bloque de la posición especificada.
 	public Bloque getBloque(Posicion pos) {
 		return config.getBloque(pos);
 	}
 	
+	//Destruye el bloque de la posición especificada.
 	public void destruirBloque(Posicion pos) {
 		config.destruirBloque(pos);
 	}
 	
+	//Devuelve true si el casillero de la posición especificada está vacío, false en caso contrario.
 	public boolean casilleroVacio(Posicion posicion) {
 		return config.casilleroVacio(posicion);
 	}
 	
+	//Devuelve el alto del suelo.
 	public int getAlto() {
 		return config.getAlto();
 	}
